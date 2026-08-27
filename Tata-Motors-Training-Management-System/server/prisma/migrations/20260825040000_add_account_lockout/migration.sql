42d54ad0ed8e9909b6a3c65761542aa6111779bc
Harsh Ravindra Sawant <harshsawant2027@gmail.com>
2026-08-27 16:50:00 +0530

    Delete Tata-Motors-Training-Management-System/server/prisma/migrations/20260825040000_add_account_lockout/migration.sql

--- Tata-Motors-Training-Management-System/server/prisma/migrations/20260825040000_add_account_lockout/migration.sql
+++
@@ -1,5 +0,0 @@
--- Adds brute-force protection fields to User: a running count of consecutive
--- failed login attempts, and a timestamp until which the account is locked
--- out after too many failures.
-ALTER TABLE "User" ADD COLUMN "failedLoginAttempts" INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE "User" ADD COLUMN "lockedUntil" DATETIME;
